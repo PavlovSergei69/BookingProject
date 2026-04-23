945c28e0ceaa94e1947cb16a3e348ea02ad29e62
pavlov-s <pavlov-s@datagile.ru>
2026-04-23 16:45:16 +0300

    Второй урок. Исправление ошибок с лекции. От 23.04.2026

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -1,45 +1,84 @@
+from http.client import responses
+
 import requests
 import os
 from dotenv import load_dotenv
-from core.settings.environments import Environments
+from core.settings.environments import Environment
+from core.settings.config import Users, Timeout
+from core.clients.endpoints import Endpoints
+import allure
 
-load_dotenv()
+
+load_dotenv()   #прогрузка всех актуальных переменных с файла .env
+
 
 class APIClient:
+    #использование окружающей среды: тестовой или продуктовой
+    #api_clients -> environments -> env -> test or prod
     def __init__(self):
         environment_str = os.getenv('ENVIRONMENT')
+        #Edit configurations-> Autodetest-> Environment variables: ENVIRONMENT=TEST, вытягивает с данного значения TEST
         try:
-            environment_str = Environment[environment_str]
+            environment = Environment[environment_str]
         except KeyError:
             raise ValueError(f"Unsupported environment value: {environment_str}")
+            #Если мы не находимый нужный ключ, то "Неподдерживаемое значение среды"
 
-        self.base_url = self.get_base_url(environment_str)
-        self.headers = {
+        self.base_url = self.get_base_url(environment)
+        self.session = requests.Session()
+        #Настраивает сессию так, чтобы все её HTTP‑запросы содержали заголовок Content-Type: application/json
+        self.session.headers = {
             'Content-Type':'application/json'
         }
 
-    # str возвращает значение в строке
-    def get_base_url(self, environment: Environments) -> str:
+    def get_base_url(self, environment: Environment) -> str:
+        # str возвращает значение в строке
+
         if environment == Environment.TEST:
             return os.getenv('TEST_BASE_URL')
         elif environment == Environment.PROD:
             return os.getenv('PROD_BASE_URL')
         else:
-            raise ValueError(f'Unsupported environment: {environment}')
+            raise ValueError(f'Unsupported environment: {environment}') #Неподдерживаемая среда
 
     def get(self, endpoint, params=None, status_code=200):
-        url = self.base_url + ednpoint
+        url = self.base_url + endpoint
         response = requests.get(url,headers=self.headers, params=params)
         if status_code:
-            assert response.status_code == status_code
+            assert response.status_code == status_code  #assert=утверждаем
         return response.json()
 
     def post(self, endpoint, data=None, status_code=200):
-        url = self.base_url + ednpoint
-        response = requests.get(url,headers=self.headers, json=data)
+        url = self.base_url + endpoint
+        response = requests.post(url,headers=self.headers, json=data)
         if status_code:
             assert response.status_code == status_code
         return response.json()
+
+    def ping(self):
+        with allure.step('Ping api client'):
+            url = f"{self.base_url}{Endpoints.PING_ENDPOINT}"
+            response = self.session.get(url)
+            response.raise_for_status()
+            #Доп.проверка. Вызывает исключение: class:`HTTPError`, если таковое произошло.
+        with allure.step('Assert status code'):
+            assert response.status_code == 201, f"Expected status 201 but got {response.status_code}"
+            return response.status_code
+
+    def auth(self):
+        with allure.step('Getting authenticate'):
+            url = f"{self.base_url}{Endpoints.AUTH_ENDPOINT}"
+            payload = {"username": Users.USERNAME, "password": Users.PASSWORD}
+            response = self.session.post(url, json=payload, timeout=Timeout.TIMEOUT)
+            response.raise_for_status()
+        with allure.step('Checking status code'):
+            assert response.status_code == 200, f"Expected status 200 but got {response.status_code}"
+            token = response.json().get("token")
+            with allure.step('Updating header with authorization'):
+                #Обновление заголовка с авторизацией
+                self.session.headers.update({"Authorization": f"Bearer {token}"})
+
+    def get_booking_by_id
 
 
 
